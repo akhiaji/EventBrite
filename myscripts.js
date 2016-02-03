4ce93705e218862438f268f7be2faa6dba159c36
Akhilesh Aji <akhiaji@gmail.com>
2016-02-02 22:46:26 -0500

    added documentation and cleaned code

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,4 @@
+//default function to run when document is ready
 $(document).ready(function(){
 	var events = [];
 	var pagination = 0;
@@ -6,7 +7,7 @@ $(document).ready(function(){
 	var lng = 0;
 	var currentLocation = true;
 
-	
+//get request for events around current location
 	function showPosition(position) {
     	lat =  position.coords.latitude
     	lng = position.coords.longitude	
@@ -29,6 +30,7 @@ $(document).ready(function(){
 					$("#open").children().hide()
 		});
 	}
+//display table with 5 events per page
 	function display() {
 		var children = $("#table").children("tbody").children("tr");
 		console.log(children.length)
@@ -52,6 +54,7 @@ $(document).ready(function(){
 			console.log(events[pagination+i].name.text)				
 		}
 	}
+//get venue to display location of event in details page
 	function getVenue(id) {
 		var sFormat = "https://www.eventbriteapi.com/v3/venues/{0}/";
 		var result = bob.string.formatString(sFormat, id);
@@ -62,7 +65,7 @@ $(document).ready(function(){
 				venue = data
 		});
 	}
-	
+//toggle between current location and form
 	$("#otherLocation").click(function(){
 		if(currentLocation) {		
 			$("#form").show();
@@ -76,7 +79,7 @@ $(document).ready(function(){
 
 		
 	});
-
+//initiate query for events
     $("#checkPage").click(function(){
 		pagination = 0;
 		var address = $("#address").val()+" "+$("#city").val()+' '+$("#state").val()
@@ -108,29 +111,7 @@ $(document).ready(function(){
 		}
 	});
 
-	$("#nextWeekend").click(function(){
-		pagination = 0;
-		var address = $("#address").val()+" "+$("#city").val()+' '+$("#state").val()
-		console.log(address)
-		var d = new Date()
-		5-d.getDay() 
-        $.get("https://www.eventbriteapi.com/v3/events/search/",
-			{'popular':'on',
-			'location.address': address,
-			'location.within':"50mi",
-			'start_date.keyword':'this_weekend',
-			'token':"6VMARHH2OSFVUIRK26OJ" },
-			function(data, status, xhr) {
-				events = data.events
-				display()
-				console.log(window.width)
-				window.resizeTo(window.height, 700);
-				console.log(window.width)
-				$("#table").show()
-				$("#fwdBck").children().show()
-			 });
-	});
-
+//function for getting details on a specific event
 	$(".clickable").click(function(){
 		$("#table").hide()
 		$("#fwdBck").children().hide()
@@ -158,18 +139,22 @@ $(document).ready(function(){
 		});
 	});
 
+//function to go back into table view
 	$("#exitDescription").click(function(){
 		$("#table").show()
 		$("#fwdBck").children().show()
 		$("#open").hide()
 	});
-	
+
+//button to advance to the next set of 5 results
 	$("#nextButton").click(function(){
 		if (pagination <= events.length - 5) {
 			pagination = pagination + 5
 			display()
 		}
 	});
+
+//button to go to the previous set of results
 	$("#backButton").click(function(){
 		if (pagination >= 5) {
 			pagination = pagination - 5
@@ -177,20 +162,21 @@ $(document).ready(function(){
 		}
 	});
 
+//method to keep the rangeBox linked to the slider
 	$("#slider").change(function(){
 		if($("#rangeBox").val()!=$(this).val()) {
 			$("#rangeBox").val($(this).val())
 		}
 	});
+
+
+//method to keep the slider linked to the rangeBox
 	$("#rangeBox").change(function(){
 		if($("#slider").val()!=$(this).val()) {
 			$("#slider").val($(this).val())
 		}
 	
 	});
-	$("form").submit(function(){
-        alert("Submitted");
-    });
 
 });
 
